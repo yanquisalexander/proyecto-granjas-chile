6651a43bf1b7b944c5d80862f27d1062a40f387d
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2024-02-24 11:22:13 -0300

    dev: filter sobe params on logs

--- backend/app/middlewares/LogRequests.middleware.ts
+++ backend/app/middlewares/LogRequests.middleware.ts
@@ -1,8 +1,33 @@
-import { type Request, type Response, type NextFunction } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import { Loggers } from '../initializers/create_loggers'
+
+const PARAMS_TO_FILTER = [
+  'password',
+  'password_confirmation',
+  'token'
+]
+
+function filterSensitiveParams (data: any): any {
+  // Recorre las claves sensibles y filtra su valor en el objeto de datos
+  PARAMS_TO_FILTER.forEach((param) => {
+    if (data[param]) {
+      data[param] = 'FILTERED'
+    }
+  })
+
+  return data
+}
 
 export default function LogRequestsMiddleware (req: Request, res: Response, next: NextFunction) {
   Loggers.WebServer.writeLog(`[${req.method}] ${req.originalUrl}`)
-  console.log(`[${req.method}] ${req.originalUrl}`)
+
+  // Si es una solicitud POST o PUT, filtra los parámetros sensibles en el cuerpo
+  if (req.method === 'POST' || req.method === 'PUT') {
+    const filteredBody = filterSensitiveParams({ ...req.body })
+    Loggers.WebServer.writeLog(`[${req.method}] ${req.originalUrl} - Body: ${JSON.stringify(filteredBody)}`)
+  }
+
+  // También puedes agregar lógica para filtrar parámetros en otros lugares, como en los parámetros de consulta (req.query)
+
   next()
 }
