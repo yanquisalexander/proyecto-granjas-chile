24bbd60d5dc7d34ce9df547f0d92be4d7910873f
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2024-02-24 11:22:24 -0300

    feat: clear old log files

--- backend/lib/Logger.ts
+++ backend/lib/Logger.ts
@@ -1,3 +1,4 @@
+import { Configuration } from '@/config'
 import * as fs from 'node:fs'
 import * as path from 'node:path'
 
@@ -12,6 +13,7 @@ class Logger {
     if (!fs.existsSync(logsDirectory)) {
       fs.mkdirSync(logsDirectory)
     }
+
     const timestamp = new Date().toISOString().replace(/[-T:]/g, '').split('.')[0]
 
     if (logFileName) {
@@ -19,14 +21,43 @@ class Logger {
     } else {
       this.logFilePath = path.join(logsDirectory, `log_${timestamp}.txt`)
     }
+
+    // Llama a clearOldLogs después de asegurarse de que el directorio existe
+    this.clearOldLogs({
+      logsToKeep: Configuration.MAX_LOGS_TO_KEEP,
+      filePrefix: logFileName
+    })
   }
 
   writeLog (message: string): void {
     const logMessage = `[${new Date().toLocaleString()}] ${message}\n`
+    console.log(logMessage)
     fs.appendFile(this.logFilePath, logMessage, (err) => {
       if (err) {
         console.error('Error writing to log file:', err)
       }
+    })
+  }
+
+  clearOldLogs ({ logsToKeep = 5, filePrefix = 'log' } = {}): void {
+    const logDirectory = path.dirname(this.logFilePath)
+
+    fs.readdir(logDirectory, (err, files) => {
+      if (err) {
+        console.error('Error reading log files:', err)
+        return
+      }
+
+      const logFiles = files.filter((file) => file.startsWith(filePrefix) && file.endsWith('.txt'))
+      logFiles.sort().reverse()
+
+      logFiles.slice(logsToKeep).forEach((file) => {
+        fs.unlink(path.join(logDirectory, file), (err) => {
+          if (err) {
+            console.error('Error deleting log file:', err)
+          }
+        })
+      })
     })
   }
 }
