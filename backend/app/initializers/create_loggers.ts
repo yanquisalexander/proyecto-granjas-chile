186c5785cc9e569681fb5916b17af21a7f147962
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2024-02-23 23:41:42 -0300

    feat: create application loggers

---
+++ backend/app/initializers/create_loggers.ts
@@ -0,0 +1,5 @@
+import Logger from '@/lib/Logger'
+
+export const Loggers = {
+  Database: new Logger('database')
+}
---
+++ backend/lib/Logger.ts
@@ -0,0 +1,35 @@
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+
+const __dirname = path.resolve()
+
+class Logger {
+  private readonly logFilePath: string
+
+  constructor (logFileName?: string) {
+    const logsDirectory = path.join(__dirname, 'logs')
+
+    if (!fs.existsSync(logsDirectory)) {
+      fs.mkdirSync(logsDirectory)
+    }
+    const timestamp = new Date().toISOString().replace(/[-T:]/g, '').split('.')[0]
+
+    if (logFileName) {
+      this.logFilePath = path.join(logsDirectory, `${logFileName}_${timestamp}.txt`)
+    } else {
+      this.logFilePath = path.join(logsDirectory, `log_${timestamp}.txt`)
+    }
+  }
+
+  writeLog (message: string): void {
+    const logMessage = `[${new Date().toLocaleString()}] ${message}\n`
+
+    fs.appendFile(this.logFilePath, logMessage, (err) => {
+      if (err) {
+        console.error('Error writing to log file:', err)
+      }
+    })
+  }
+}
+
+export default Logger
