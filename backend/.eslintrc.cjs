a65cf79e04fdb331b9de07b127971ebb485c6a66
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2024-02-26 23:22:30 -0300

    Revert "fix: typescript error on eslint config"
    This reverts commit 5135b586cb8803719eee13a7f371ffabc700bca1.

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -1,39 +1,41 @@
 module.exports = {
-  parserOptions: {
-    project: [
-      './tsconfig.json'
+    parserOptions: {
+        project: [
+            './tsconfig.json',
+            './.eslintrc.cjs'
+        ],
+    },
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": "standard-with-typescript",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
     ],
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  root: true,
-  env: {
-    es2021: true,
-    node: true
-  },
-  extends: 'standard-with-typescript',
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
-      parserOptions: {
-        sourceType: 'script'
-      }
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        "@typescript-eslint/no-unsafe-argument": "off",
+        "@typescript-eslint/strict-boolean-expressions": "off",
+        "@typescript-eslint/naming-convention": "off",
+        "@typescript-eslint/prefer-nullish-coalescing": "off",
+        "@typescript-eslint/no-extraneous-class": "off",
+        "@typescript-eslint/no-floating-promises": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/consistent-type-imports": "off",
+        "@typescript-eslint/no-misused-promises": "off",
     }
-  ],
-  rules: {
-    '@typescript-eslint/no-unsafe-argument': 'off',
-    '@typescript-eslint/strict-boolean-expressions': 'off',
-    '@typescript-eslint/naming-convention': 'off',
-    '@typescript-eslint/prefer-nullish-coalescing': 'off',
-    '@typescript-eslint/no-extraneous-class': 'off',
-    '@typescript-eslint/no-floating-promises': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/consistent-type-imports': 'off',
-    '@typescript-eslint/no-misused-promises': 'off'
-  }
 }
