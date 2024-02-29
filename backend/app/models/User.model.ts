69dcfd65d2521e394316367310dc4ebbff78b019
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2024-02-28 21:07:22 -0300

    ...

--- backend/app/models/User.model.ts
+++ backend/app/models/User.model.ts
@@ -1,4 +1,4 @@
-import Model from '@/lib/Model'
+import Database from '@/lib/DatabaseManager'
 import { UUID } from 'crypto'
 
 interface UserAttributes {
@@ -10,62 +10,38 @@ interface UserAttributes {
   updated_at?: Date
 }
 
-class User extends Model {
+class User {
+  id: UUID
+  username: string
+  email: string
+  password: string
+  created_at?: Date
+  updated_at?: Date
+
   constructor (attributes: UserAttributes) {
-    super('users', 'id', attributes)
-  }
-
-  get id (): UUID {
-    return this.attributes.id
-  }
-
-  get username (): string {
-    return this.attributes.username
-  }
-
-  set username (value: string) {
-    this.attributes.username = value
-  }
-
-  get email (): string {
-    return this.attributes.email
-  }
-
-  set email (value: string) {
-    this.attributes.email = value
-  }
-
-  get password (): string {
-    return this.attributes.password
-  }
-
-  set password (value: string) {
-    this.attributes.password = value
-  }
-
-  get created_at (): Date {
-    return this.attributes.created_at
-  }
-
-  set created_at (value: Date) {
-    this.attributes.created_at = value
-  }
-
-  get updatedAt (): Date {
-    return this.attributes.updated_at
-  }
-
-  set updatedAt (value: Date) {
-    this.attributes.updated_at = value
+    this.id = attributes.id
+    this.username = attributes.username
+    this.email = attributes.email
+    this.password = attributes.password
+    this.created_at = attributes.created_at
+    this.updated_at = attributes.updated_at
   }
 
   async save (): Promise<void> {
-    if (this.attributes.id) {
-      await this.update()
+    const user = await Database.query('SELECT * FROM users WHERE id = $1', [this.id])
+    if (user) {
+      await Database.query('UPDATE users SET username = $1, email = $2, password = $3 WHERE id = $4', [this.username, this.email, this.password, this.id])
     } else {
-      const result = await super.create(this.attributes)
-      Object.assign(this.attributes, result.attributes)
+      await Database.query('INSERT INTO users (id, username, email, password) VALUES ($1, $2, $3, $4)', [this.id, this.username, this.email, this.password])
     }
+  }
+
+  static async find (id: UUID): Promise<User | null> {
+    const result = await Database.query('SELECT * FROM users WHERE id = $1', [id])
+    if (result.rows.length === 0) {
+      return null
+    }
+    return new User(result.rows[0])
   }
 }
 
