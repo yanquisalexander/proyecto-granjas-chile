e7a072987774d9d8b23c1e8166bd7a2b65b412c8
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2024-03-04 12:25:41 -0300

    ux: login page

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="min-h-screen flex w-full flex-col justify-center items-center">
-        <div class="min-w-72 mx-4 shadow-md border border-solid rounded-lg p-4 py-12">
+    <div
+        class="min-h-screen flex w-full flex-col justify-center items-center bg-[url('/images/bg-login.png')] bg-cover bg-center bg-no-repeat">
+        <div class="min-w-72 mx-4 shadow-md border border-solid rounded-lg p-4 py-12 bg-white dark:bg-gray-800">
             <header class="text-center my-6">
-                <img src="/images/granja-regando.webp" alt="Logo sitio administradores" class="w-20 mx-auto" />
+                <img draggable="false" src="/images/granja-regando.webp" alt="Logo sitio administradores"
+                    class="w-20 mx-auto" />
                 <h1 class="text-xl font-bold">
                     Sign in
                 </h1>
@@ -19,23 +21,65 @@
                     <UInput v-model="state.password" type="password" />
                 </UFormGroup>
 
+                <div class="flex space-x-4 justify-center">
 
-                <UButton type="submit" :loading="loading">
-                    Submit
-                </UButton>
+                    <UButton type="submit" :loading="loading">
+                        Sign in
+                    </UButton>
+
+                    <UButton type="button" variant="ghost" @click="dialogResetPassword = true">
+                        Reset password
+                    </UButton>
+                </div>
             </UForm>
         </div>
+
+        <USelect v-model="state.language" :options="languages" option-attribute="name" label="Language" class="mt-4" />
+
+
+        <UModal v-model="dialogResetPassword">
+            <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
+                <template #header>
+                    <h2 class="text-xl font-bold">
+                        Reset password
+                    </h2>
+                </template>
+
+                <p class="p-4">
+                    Si tu cuenta existe, y tiene acceso de administrador, se enviará un correo con un enlace para
+                    restablecer tu
+                    contraseña.
+                </p>
+
+                <p class="p-4">
+                    El correo ingresado es: <strong>{{ state.email }}</strong>
+                </p>
+
+
+
+                <template #footer>
+                    <div class="flex justify-end space-x-4">
+                        <UButton type="button" color="blue" variant="ghost" @click="dialogResetPassword = false">
+                            Cancel
+                        </UButton>
+                        <UButton type="button" color="blue" @click="sendResetPassword">
+                            Reset
+                        </UButton>
+                    </div>
+                </template>
+            </UCard>
+        </UModal>
     </div>
 </template>
 
 <script setup lang="ts">
-import type { FetchResult } from "#app";
 
 const { signIn } = useAuth()
 
 const state = reactive({
     email: '',
-    password: ''
+    password: '',
+    language: 'en'
 })
 
 watch(() => state.email, (newVal) => {
@@ -43,6 +87,7 @@ watch(() => state.email, (newVal) => {
 })
 
 const loading = ref(false)
+const dialogResetPassword = ref(false)
 const error = ref<string | null>(null)
 
 const handleSubmit = async () => {
@@ -71,10 +116,22 @@ const handleSubmit = async () => {
     } finally {
         loading.value = false
     }
-
-
-
 }
+
+const sendResetPassword = () => {
+    console.log("sendResetPassword")
+    dialogResetPassword.value = false
+}
+
+const languages = ref([
+    {
+        name: 'English',
+        value: 'en'
+    }, {
+        name: 'Spanish',
+        value: 'es'
+    }
+])
 
 definePageMeta({
     auth: {
